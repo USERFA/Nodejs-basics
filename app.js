b53fa45ca73f66ddbfbaa9eeca806869c5bda0e4
Fadwa <faaadwaaa05@gmail.com>
2025-06-25 10:46:33 +0100

    blocking code example

--- app.js
+++ app.js
@@ -102,7 +102,32 @@
 
 
 
-const _ = require('lodash');
-const items = [1,[2,[3,[4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+// const _ = require('lodash');
+// const items = [1,[2,[3,[4]]]]
+// const newItems = _.flattenDeep(items);
+// console.log(newItems);
+
+
+
+//blocking code
+const http = require("http");
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.end("Home page")
+    }
+    if (req.url === "/about") {
+        //blocking code (nested for loop) //once the loop finishes then the page is rendered
+        for (let i = 0; i < 100; i++) {
+            for (let j = 0; j < 100; j++) {
+                console.log(i, j);
+
+            }
+        }
+        res.end("About page")
+    }
+    res.end("Error page")
+
+});
+server.listen("5000", () => {
+    console.log("server is listenning on port 5000");
+});
